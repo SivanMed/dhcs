1e848da234ff7c86877c60263418390fc0b67a44
MPesin <mikelpesin@gmail.com>
2017-05-18 02:37:28 +0300

    part 3 awesome

--- Ass1/ass1.py
+++ Ass1/ass1.py
@@ -3,12 +3,8 @@ import sys
 import codecs
 import re
 
-#main
-OriginalT = 'input.txt'
-originalText = codecs.open(OriginalT,'r',encoding='UTF-8')
+###################### XML Definitions ######################
 
-output = 'output.xml'
-ans =codecs.open(output,'w',encoding='UTF-8')
 meta = """<?xml version="1.0" encoding="UTF-8"?>
 <?xml-model href="http://www.tei-c.org/release/xml/tei/custom/schema/relaxng/tei_all.rng" type="application/xml" schematypens="http://relaxng.org/ns/structure/1.0"?>
 <?xml-model href="http://www.tei-c.org/release/xml/tei/custom/schema/relaxng/tei_all.rng" type="application/xml"
@@ -37,34 +33,44 @@ closing="""</body>
 </text>
 </TEI>"""
 
-tag_list = list()
+###################### Initialization ######################
 
-str = codecs.open('input.txt', 'r',encoding="utf8" ).read()
-inner = ""
-nl = ""
+originalText = codecs.open('input.txt','r',encoding='UTF-8')
+ans = codecs.open('output.xml','w',encoding='UTF-8')
+tag_dict = dict()
+body = ""
+
+###################### Tagging ######################
+
+def scanTags ():
+	with codecs.open ('TagSource.txt', 'r',encoding="utf8" ) as tagged_list:
+		for line in tagged_list:
+			line_words = line.split()
+			if line_words:
+				word = line_words[1]
+				tag = line_words[4]
+				if not word in tag_dict:
+					tag_dict[word] = tag
+
+def TagWord (word):
+	if word in tag_dict:
+		tag = tag_dict[word]
+		word = "<" + tag + ">" + word + "</" + tag + ">"
+	if re.findall(r'\d\d\d\d|\d\d.\d.\d\d\d\d|\d\d.\d\d.\d\d\d\d', word):
+		word = "<date>" + word + "</date>"
+	word = " " + word + " "
+	return word
+
+###################### main ######################
+
+scanTags()
 for line in originalText:
 		text = line.split()
 		for Origin_word in text:
-			with codecs.open('output.txt',encoding="utf8") as source:
-				for source_line in source:
-					if (Origin_word in source_line) and ("properName" in source_line):
-						Origin_word="<properName>"+ Origin_word +"</properName>"+"\n"
-						break
-					if ("," in Origin_word) and (Origin_word[:-1] in source_line) and ("properName" in source_line):
-						Origin_word = "<properName>" + Origin_word + "</properName>" + "\n"
-						break
+			body = body + TagWord (Origin_word)
 
-			if (re.findall(r'\d\d\d\d', Origin_word)):
-				Origin_word= "<Date>" + Origin_word +"</Date>" +"\n"
-			nl = " " + Origin_word + " "
-			inner = inner + nl
+###################### return ######################
 
-
-
-outputText=meta+"\n"+inner+"\n"+closing
-
+outputText=meta+"\n"+body+"\n"+closing
 ans.write(outputText)
-
-#def scanForTags (file, tag_list):
-#	txt = codecs.open ('output.txt', 'r',encoding="utf8" ).read()
 	
